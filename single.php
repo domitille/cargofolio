b5daa1e73ebfac493b4f98c3b74adea4bcd77537
gaspard <gaspard@gmail.com>
2017-12-08 13:07:43 +0100

    single: display other posts

--- single.php
+++ single.php
@@ -57,6 +57,40 @@
     wp_reset_query();
   } // if tags
   ?>
+  <!-- display other references -->
+  <?php
+    $args_other = array(
+      'post__not_in' => $already_displayed_IDs,
+      'posts_per_page' => 12 - count($already_displayed_IDs),
+      'caller_get_posts' => 1
+    );
+    $other = new WP_Query($args_other);
+  ?>
+  <?php if ( $other->have_posts() ) : ?>
+    <?php while ($other->have_posts()) : $other->the_post(); array_push ($already_displayed_IDs, get_the_ID()); ?>
+      <article class="col-lg-4 col-md-6 col-sm-12 related">
+        <a href="<?php the_permalink(); ?>"
+          class="primary"
+          title="<?php printf( __('Go to %s', 'cargofolio'), the_title_attribute('echo=0') ); ?>"
+          rel="bookmark">
+        <?php if ( has_post_thumbnail() ) {
+          echo get_the_post_thumbnail( null, 'cargofolio-thumbnail-home', array( 'width' => 'alignleft' ) );
+        } else { ?>
+          <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="135" class="" alt="">
+        <?php
+        } ?>
+          <div class="project-name"><?php the_title(); ?></div>
+        </a>
+        <div class="tags secondary">
+        <?php if(get_the_tag_list()) {
+          echo get_the_tag_list('',', ');
+        } ?>
+        </div>
+      </article>
+    <?php endwhile; ?>
+  <?php endif;
+  wp_reset_query();
+  ?>
 </section>
 
 <?php get_footer(); ?>

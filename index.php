abc1bf3345be0c1574ff29cc3e0a21403e953754
gaspard <gaspard@gmail.com>
2017-12-17 17:12:23 +0100

    thumbnails: use custom thumbnail sizes

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
   <?php while ( have_posts() ) : the_post() ?>
   <article class="col-lg-4 col-md-6 col-sm-12">
     <a href="<?php the_permalink(); ?>"
-       title="<?php printf( __('Go to %s', 'cargofolio'), the_title_attribute('echo=0') ); ?>"
-       rel="bookmark">
+      class="primary"
+      title="<?php printf( __('Go to %s', 'cargofolio'), the_title_attribute('echo=0') ); ?>"
+      rel="bookmark">
     <?php if ( has_post_thumbnail() ) {
-      echo get_the_post_thumbnail( get_queried_object_id(), 'cargofolio-thumbnail-home' );
+      echo get_the_post_thumbnail( null, 'cargofolio-thumbnail-home', array( 'width' => 'alignleft' ) );
     } else { ?>
       <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="135" class="" alt="">
     <?php
